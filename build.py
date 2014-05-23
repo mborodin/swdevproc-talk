788b1c2f0c4a2ee4ad1921ec7708044e8737f590
Maksym Borodin <borodin.maksym@gmail.com>
2014-05-23 14:53:42 +0200

    initial commit

---
+++ build.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/svg2tikz')
+
+from svg2tikz.extensions.tikz_export import convert_svg
+from subprocess import Popen
+import logging
+
+config = {'images': os.getcwd() + '/images',
+          'texfile': 'sw_dev_process.tex'}
+
+
+def gen_images(imgpath):
+    files = [f for f in os.listdir(imgpath) if f.endswith('.svg')]
+    for fname in files:
+        texname = fname.replace('.svg', '.tex')
+        with open(texname, 'w') as fout:
+            fout.write(convert_svg(fname, figonly=True))
+
+
+def compile_pdf(texfile):
+    proc = Popen(['pdflatex', texfile])
+    proc.communicate()
+
+
+def cleanup(texfile, imgpath):
+    os.unlink(texfile.replace('.tex', '.aux'))
+    os.unlink(texfile.replace('.tex', '.log'))
+    [os.unlink(f) for f in os.listdir(imgpath) if f.endswith('.tex')]
+
+
+def main():
+    if not os.path.exists(config['texfile']):
+        logging.error("Can't find TeX input %s" % config['texfile'])
+        return
+    if not os.path.exists(config['images']):
+        logging.warning('Image directory %s is not accessible'
+                        % config['images'])
+    else:
+        gen_images(config['images'])
+    compile_pdf(config['texfile'])
+    cleanup(config['texfile'], config['images'])
+
+
+if __name__ == '__main__':
+    main()
